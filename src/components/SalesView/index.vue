50c6266f75c3448a048227f95c3026c03f0cf694
robby.yin <robby.yin@valorosoltd.com>
2021-01-21 01:05:40 +0800

    第二个View开发完毕

--- src/components/SalesView/index.vue
+++ src/components/SalesView/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="sales-view">
     <!--  1：创建一个card样式组件  -->
-    <el-card shadow="hover">
+    <el-card shadow="hover" :body-style="{ padding: '0 0 20px 0' }">
       <!-- 在Card样式组件里面，name为header的slot，可以将Card组件分为两个部分，一个是头部一个内容 -->
       <template v-slot:header>
         <div class="menu-wrapper">
@@ -42,9 +42,27 @@
           </div>
         </div>
       </template>
-      <!-- 这里是将内容插入到子组件的 -->
+      <!-- 这里是将内容插入到子组件的, 这里要查一下，为什么Card没有slot也可以使用template -->
       <template>
-        <div>这是内容</div>
+        <div class="sales-view-chart-wrapper">
+          <!-- 左边部分是图 -->
+          <v-chart :options="chartOption"/>
+          <!-- 右边部分是列表 -->
+          <div class="sales-view-list">
+            <div class="sales-view-title">排行榜</div>
+            <div class="list-item-wrapper">
+              <div class="list-item" v-for="item in rankData" :key="item.no">
+                <!-- 这里的class是动态绑定的，后面是一个数组，且第二个元素还是一个三元运算符
+                那么前面三个item的class=list-item-no top-no， 后面的元素class=list-item-no
+                如果是动态绑定:style样式，也可以这样使用数组，将样式合并
+                 -->
+                <div :class="['list-item-no', +item.no <= 3 ? 'top-no' : '']">{{item.no}}</div>
+                <div class="list-item-name">{{item.name}}</div>
+                <div class="list-item-money">{{item.money}}</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </template>
     </el-card>
   </div>
@@ -96,7 +114,101 @@
               }
             }
           ]
-        }
+        },
+        chartOption: {
+          title: {
+            text: '年度销售额',
+            textStyle: {
+              fontSize: 12,
+              color: '#666'
+            },
+            left: 25,
+            top: 20,
+          },
+          xAxis: {
+            type: 'category',
+            data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+            axisTick: {
+              alighWithLabel: true,
+              lineStyle: {
+                color: '#999'
+              }
+            },
+            axisLine: {
+              lineStyle: {
+                color: '#999'
+              }
+            },
+            axisLabel: {
+              color: '#333'
+            }
+          },
+          yAxis: {
+            axisLine: {
+              show: false
+            },
+            axisTick: {
+              show: false
+            },
+            splitLine: {
+              lineStyle: {
+                type: 'dotted',
+                color: '#eee'
+              }
+            }
+          },
+          series: [
+            {
+              type: 'bar',
+              barWidth: '35%',
+              data: [200, 250, 300, 350, 200, 250, 300, 350, 200, 250, 300, 350, ]
+            }
+          ],
+          color: ['#339874'],
+          grid: {
+            top: 70,
+            left: 60,
+            right: 60,
+            bottom: 50
+          }
+        },
+        rankData: [
+          {
+            no: 1,
+            name: '麦当劳',
+            money: '234,234'
+          },
+          {
+            no: 2,
+            name: '麦当劳',
+            money: '234,234'
+          },
+          {
+            no: 3,
+            name: '麦当劳',
+            money: '234,234'
+          },
+          {
+            no: 4,
+            name: '麦当劳',
+            money: '234,234'
+          },
+          {
+            no: 5,
+            name: '麦当劳',
+            money: '234,234'
+          },
+          {
+            no: 6,
+            name: '麦当劳',
+            money: '234,234'
+          },
+          {
+            no: 7,
+            name: '麦当劳',
+            money: '234,234'
+          }
+        ]
       }
     },
     methods: {
@@ -149,5 +261,68 @@
   // 3：这个样式控制View中的头部
   .menu-wrapper {
     display: flex;
+  }
+  // 4：内容部分
+  .sales-view-chart-wrapper {
+    display: flex;
+    height: 270px;
+    // 左边图表
+    .echarts {
+      flex: 0 0 70%;
+      width: 70%;
+      height: 100%;
+    }
+    // 右边列表
+    .sales-view-list {
+      flex: 1;
+      width: 100%;
+      height: 100%;
+      overflow: hidden;
+
+      // 标题样式
+      .sales-view-title {
+        margin-top: 20px;
+        font-size: 12px;
+        color: #666666;
+        font-weight: 500;
+      }
+      .list-item-wrapper {
+        margin-top: 15px;
+        .list-item {
+          display: flex;
+          align-items: center;
+          font-size: 12px;
+          height: 20px;
+          padding: 6px 20px 6px 0;
+
+          // 不是前3的列表
+          .list-item-no {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 20px;
+            height: 20px;
+            color: #333;
+
+            // 前3的列表
+            &.top-no {
+              background: #000;
+              border-radius: 50%;
+              color: #fff;
+              font-weight: 500;
+            }
+          }
+          .list-item-name {
+            margin-left: 10px;
+            color: #333;
+          }
+          .list-item-money {
+            flex: 1;
+            text-align: right;
+          }
+        }
+      }
+
+    }
   }
 </style>
